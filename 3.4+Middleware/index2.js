f2883ad01afeb55797fc5f3d9b4823a7c8996f4a
Krupakar <krupakarbadugu2007@gmail.com>
2026-04-29 23:46:37 +0530

    Organize Node learning progress

---
+++ 3.4+Middleware/index2.js
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+const port = 3001;
+
+const ans = morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+})
+
+app.use(ans);
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
